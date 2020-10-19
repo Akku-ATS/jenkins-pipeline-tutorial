de882351f4947a96e3a56fd149a8230024e0aa52
Achuet414 <32358392+Achuet414@users.noreply.github.com>
2020-10-19 15:48:12 +0530

    Update search.py

--- search.py
+++ search.py
@@ -2,7 +2,7 @@
 This module contains DuckDuckGoSearchPage,
 the page object for the DuckDuckGo search page.
 """
-
+test 1910
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
