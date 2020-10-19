3f2302d7d1a3568e27da94624e8d97ddb30cc4bd
Achuet414 <32358392+Achuet414@users.noreply.github.com>
2020-10-19 15:32:29 +0530

    Create bug.py

---
+++ bug.py
@@ -0,0 +1,34 @@
+   #! /usr/bin/env python
+ 
+    'Example errors caught by PyChecker'
+ 
+    import string
+ 
+    metaslash = 1
+ 
+    def printNames():
+       neal = 'neal'
+       michelle = 'michele'
+       eric = 5
+       print "Local values: %(neal)S %(michele)s %(eric)" % locals()
+
+   class Nothing:
+       def printValue(value):
+           print value
+       def set(self, value):
+           self.value = value
+
+   def tryToDoSomething(self, value):
+       try:
+           import string
+           if not value:
+               raise RuntimeError, "Hey, there's no value"
+           printNames('a, b, c')
+       except:
+           traceback.print_exc()
+
+   def setGlobal(value=None):
+       print 'Old MetaSlash value is:', metaslash
+       metaslash = value
+       useless = Nothing(5)
+       print 'a useless value is:', useless.valeu
